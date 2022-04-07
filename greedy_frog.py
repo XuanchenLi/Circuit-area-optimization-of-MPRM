547d43c18d8ef09f25a0ae58ba8769f9ca40452e
Dazz1e <2468691031@qq.com>
2022-04-07 23:23:21 +0800

    optimize

--- greedy_frog.py
+++ greedy_frog.py
@@ -1,6 +1,10 @@
 import numpy as np
 
 
+def test(inputs, w, limits):
+    return np.mat(inputs) * np.mat(w).T <= limits
+
+
 class GreedyFrog:
     def __init__(self, population, meme_size, weights, values, limitation):
         self.population = population
@@ -14,6 +18,7 @@ class GreedyFrog:
         self.global_best = None
         self.density = np.array(values).astype(np.float) / (np.array(weights).astype(np.float) + 1e-5)
         self.sorted_density_idx = np.argsort(-self.density).flatten()
+        self.sorted_weight_idx = np.argsort(-self.weights).flatten()
 
 
     def init(self):
@@ -27,28 +32,29 @@ class GreedyFrog:
         frogs = frogs.reshape(-1, self.dim)
         for i in range(frogs.shape[0]):
             if self.sum_weight(frogs[i]) > self.limitation:
-                frogs[i] = self.add(self.drop(frogs[i]))
-
+                frogs[i] = self.drop(frogs[i])
+                frogs[i] = self.add(frogs[i])
         return np.mat(frogs) * np.mat(self.values).T, frogs
 
     def drop(self, frogs):
         w = self.sum_weight(frogs)[0]
         idx = self.dim - 1
         while w > self.limitation:
-            while frogs[self.sorted_density_idx[idx]] == 0 and idx >= 0:
-                idx -= 1
-            frogs[idx] = 0
+            while frogs[self.sorted_density_idx[idx]] == 0 and idx > 0:
+                idx = idx - 1
+            frogs[self.sorted_density_idx[idx]] = 0
             w -= self.weights[self.sorted_density_idx[idx]]
+        # print(test(frogs, self.weights, self.limitation))
         return frogs
 
     def add(self, frogs):
         w = self.sum_weight(frogs)[0]
         idx = 0
         while idx < self.dim and w < self.limitation:
-            if frogs[self.sorted_density_idx[idx]] == 0 and \
-                    w + self.weights[self.sorted_density_idx[idx]] < self.limitation:
-                w += self.weights[self.sorted_density_idx[idx]]
-                frogs[self.sorted_density_idx[idx]] = 1
+            if frogs[self.sorted_weight_idx[idx]] == 0 and \
+                    w + self.weights[self.sorted_weight_idx[idx]] < self.limitation:
+                w += self.weights[self.sorted_weight_idx[idx]]
+                frogs[self.sorted_weight_idx[idx]] = 1
             idx += 1
         return frogs
 
@@ -63,8 +69,6 @@ class GreedyFrog:
         self.groups = self.fitness_sub.reshape((self.meme_size, self.group_num)).T
 
     def evolve(self, iterator_times):
-        max_t = max(1, int(iterator_times * 0.1))
-        cur_t = 0
         global_best = self.groups[0][0]
         for i in range(self.group_num):
             for iter in range(iterator_times):
@@ -75,9 +79,10 @@ class GreedyFrog:
                         local_best = self.groups[i][k]
                     if self.fitness[self.groups[i][k]] < self.fitness[local_worst]:
                         local_worst = self.groups[i][k]
-                dis = (np.random.rand() *
-                       (self.frogs[local_best] - self.frogs[local_worst])) \
-                    .round().astype(np.int)
+
+                dis = (np.random.rand(1, self.frogs[local_best].shape[0]) *
+                       (self.frogs[local_best] - self.frogs[local_worst]))
+                dis = dis.round().astype(np.int)
                 temp = self.frogs[local_worst] + dis
                 temp[temp < 0] = 0
                 temp_f, temp = self.get_fitness_with_limit(temp)
@@ -85,7 +90,7 @@ class GreedyFrog:
                     self.frogs[local_worst] = temp
                     self.fitness[local_worst] = temp_f
                 else:
-                    dis = (np.random.rand() *
+                    dis = (np.random.rand(1, self.frogs[global_best].shape[0]) *
                            (self.frogs[global_best] - self.frogs[local_worst])) \
                         .round().astype(np.int)
                     temp = self.frogs[local_worst] + dis
@@ -96,13 +101,15 @@ class GreedyFrog:
                         self.fitness[local_worst] = temp_f
                     else:
                         self.frogs[local_worst] = np.random.randint(0, 2, size=(1, self.dim))
+                        self.fitness[local_worst], self.frogs[local_worst] = \
+                            self.get_fitness_with_limit(self.frogs[local_worst])
             for j in range(self.meme_size):
                 if self.fitness[global_best] < self.fitness[self.groups[i][j]]:
                     global_best = self.groups[i][j]
         return global_best
 
     def train(self, times):
-        max_t = max(1, int(times * 0.2))
+        max_t = max(1, int(times * 0.5))
         cur_t = 0
         global_best = []
         for i in range(times):
@@ -110,7 +117,8 @@ class GreedyFrog:
             self.sort()
             self.grouping()
             global_best = self.frogs[self.groups[0][0]]
-            new_best = self.evolve(100)
+            new_best = self.evolve(500)
+            # print(i, ":", self.fitness[new_best])
             if self.get_fitness(global_best) >= self.fitness[new_best]:
                 cur_t += 1
                 if cur_t >= max_t:
@@ -118,4 +126,5 @@ class GreedyFrog:
             else:
                 cur_t = 0
                 global_best = self.frogs[new_best]
+        # print(test(self.frogs[global_best], self.weights, self.limitation))
         return global_best, self.get_fitness(global_best)
--- main.py
+++ main.py
@@ -1,24 +1,47 @@
 import numpy as np
 from greedy_frog import GreedyFrog
+import time
+
+
+def test(inputs, w, limits):
+    return np.mat(inputs) * np.mat(w).T <= limits
+
+
+def dp(w, p, c):
+    f = [[0 for i in range(c + 1)] for i in range(n + 1)]  # 初始化
+    for i in range(1, n + 1):
+        for j in range(0, c + 1):
+            f[i][j] = f[i - 1][j]
+            if j >= w[0][i - 1]:
+                f[i][j] = max(f[i][j], f[i - 1][j - w[0][i - 1]] + p[0][i - 1])
+    return f[n][c]
 
 
 if __name__ == '__main__':
-    n = 500
+    n = 1500
     ans = 0
     best = 0
     w = np.random.randint(1, 10, (1, n))
     p = w + 5
     c = int(np.sum(w) / 2)
-    for i in range(30):
-        model = GreedyFrog(100, 10, w, p, c)
+    time_start1 = time.time()
+    print("ground truth: ", dp(w, p, c))
+    time_end1 = time.time()
+    print('time cost', time_end1 - time_start1, 's')
+
+    time_start2 = time.time()
+    for i in range(5):
+        model = GreedyFrog(20, 5, w, p, c)
         model.init()
-        res = model.train(40)
+        res = model.train(50)
         print(res[1])
         ans = ans + res[1]
         if res[1] > best:
             best = res[1]
     print("best: ", best)
-    print("ave: ", ans / 30.0)
+    print("ave: ", ans / 5.0)
+    time_end2 = time.time()
+    print('time cost', (time_end2 - time_start2) / 10, 's')
 
 
 
