17267a5c9146baf00e93d7e3136b4d6a5827c511
Aquarius <1258581682@qq.com>
2022-04-14 21:03:53 +0800

    minitem convert

--- circuit/circuit.py
+++ circuit/circuit.py
@@ -8,7 +8,28 @@ class BooleanCircuit:
         self.term_num = term_num
         self.terms = terms
 
+    def nimterm_convert(self):
+        while (True):
+            fix_num = 0
+            for i in range(self.term_num):
+                minterm = self.terms[i, :]
+                position = minterm.where()
+                fix = minterm
+                if position:
+                    fix_num += 1
+                    fix[position[0]] = 0
+                    np.append(self.terms, fix, axis=0)
+                    fix[position[0]] = 1
+                    np.append(self.terms, fix, axis=0)
+                else:
+                    np.append(self.terms)
+            if fix_num == 0:
+                # 矩阵中不存在-1跳出
+                break
+            np.delete(self.terms, np.s_[:self.term_num])  # 删除
+            self.term_num = np.shape(self.terms)[0]  # 获取新行数
 
+        self.teems = np.unique(self.terms, axis=0)  # 删除重复行
 
 class MPRM:
     def __init__(self, in_num, out_num, term_num, polarity, terms=None):
